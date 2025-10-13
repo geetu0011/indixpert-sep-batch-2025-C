7e03610b06f6cfe5401c3fa14d5929b4c4090fef
geetu0011 <geetanjalikapkoti@gmail.com>
2025-10-13 14:17:42 +0530

    even number

---
+++ test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    printf("this is all even numbers:-\n");
+    for(int a=2;a<100;a++)
+    {
+        if(a%2==0)
+        {
+            printf("%d",a);
+        }
+    } 
+    return 0;
+}
